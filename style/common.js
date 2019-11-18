8b5ea1bd43e477e567e96f486e5493afaa79cbd7
Sunny Soni <sunnysoni97@gmail.com>
2019-11-19 04:24:37 +0530

    layout finalised (only replacing images left)

--- style/common.js
+++ style/common.js
@@ -2,13 +2,16 @@ $(function(){
     $('#acadBtn').click(function(e){
     	e.preventDefault();
         $('#v-pills-tab a[href="#v-pills-tech"]').tab('show');
+        $('#mainContainer').scrollTo(0,{duration:'slow'});
     }),
     $('#techBtn').click(function(e){
         e.preventDefault();
         $('#v-pills-tab a[href="#v-pills-nontech"]').tab('show');
+        $('#mainContainer').scrollTo(0,{duration:'slow'});
     }),
     $('#workexBtn').click(function(e){
         e.preventDefault();
         $('#v-pills-tab2 a[href="#v-pills-projects"]').tab('show');
+        $('#mainContainer').scrollTo(0,{duration:'slow'});
     })
 })
